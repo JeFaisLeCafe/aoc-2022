e24189dfc77c255352901be69d6cc4b2d569ff18
Pierre-Etienne Soury <pierretienne.soury@gmail.com>
2022-12-11 18:52:11 +0100

    day_11 part 2 thanks to Marc Olxinos

--- day_11/index.js
+++ day_11/index.js
@@ -1,6 +1,31 @@
 import { readFileSync } from "fs";
 const content = readFileSync(new URL("./input", import.meta.url)).toString();
+// const arr = [23, 19, 13, 17];
+const arr = [13, 11, 2, 5, 7, 3, 19, 17];
+function gcd(a, b) {
+  if (b == 0) return a;
+  return gcd(b, a % b);
+}
 
+// Returns LCM of array elements
+function findlcm(arr, n) {
+  // Initialize result
+  let ans = arr[0];
+
+  // ans contains LCM of arr[0], ..arr[i]
+  // after i'th iteration,
+  for (let i = 1; i < n; i++) ans = (arr[i] * ans) / gcd(arr[i], ans);
+
+  return ans;
+}
+
+const lcm = findlcm(arr, arr.length);
+console.log(
+  "LCM",
+  lcm,
+  "product",
+  arr.reduce((acc, v) => acc * v, 1)
+);
 class Monkey {
   constructor(monkeyId, items, stressLevelOperation, passToMonkey) {
     this.monkeyId = monkeyId;
@@ -9,12 +34,18 @@ class Monkey {
     this.passToMonkey = passToMonkey; // function returning a monkeyId: (stressLevel) => 1/0
     this.itemsInspected = 0;
   }
+
   inspectItem(inspectedItem, worried = false) {
     // what does a Monkey do ?
     // he does weird shit, and throws items to other monkeys
     this.itemsInspected++;
     let stressLevel = this.stressLevelOperation(inspectedItem);
-    if (!worried) stressLevel = Math.floor(stressLevel / 3);
+    if (!worried) {
+      stressLevel = Math.floor(stressLevel / 3);
+    } else {
+      // black magic
+      stressLevel = stressLevel % lcm;
+    }
     return { monkeyDestinationId: this.passToMonkey(stressLevel), stressLevel };
   }
   receiveItem(item) {
@@ -22,6 +53,7 @@ class Monkey {
     return this.items;
   }
 }
+
 function getMonkeys() {
   return content.split("\n\n").map((monkey) => {
     const formatted = monkey.split("\n");
@@ -87,15 +119,28 @@ function part1() {
 
 console.log("PART 1:", part1()); // 117640
 
+function displayMonkeys(monkeys) {
+  monkeys.forEach((monkey) => {
+    console.log(
+      `Monkey ${monkey.monkeyId} inspected ${monkey.itemsInspected} items`
+    );
+  });
+}
+
 function part2() {
   const ROUNDS = 10_000;
   let monkeys = getMonkeys();
 
   for (let i = 1; i <= ROUNDS; i++) {
     monkeys = playARound(monkeys, true);
+    if (i % 1000 === 0) {
+      console.log("-------");
+      console.log("ROUND ", i);
+      displayMonkeys(monkeys);
+    }
   }
   // now we have the "final" state of the game
   // we need to find the 2 with the most inspected items, and return these numbers multiplied
   return getMonkeyBusiness(monkeys);
 }
-console.log("PART 2:", part2()); //
+console.log("PART 2:", part2()); // 30616425600
